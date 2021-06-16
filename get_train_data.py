f446a22cfe909fba6d5ff62a403dff0b1b66be8f
gsxhnd <gsxhnd@gmail.com>
2021-06-16 14:07:57 +0800

    update

--- get_train_data.py
+++ get_train_data.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def get_train_data():
-    pd.read_csv("./input/2020-06-02.csv")
+    train_data = pd.read_csv("./input/2020-06-02.csv")
+    train_data.hist(bins=50, figsize=(20, 15))
+    plt.show()
+    return train_data
+
+
+if __name__ == '__main__':
+    get_train_data()
