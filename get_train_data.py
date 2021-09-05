243e5f1c3769e00bb5045ad5e329beb4ce54862b
gsxhnd <gsxhnd@gmail.com>
2021-09-05 19:04:31 +0800

    update

--- get_train_data.py
+++ get_train_data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import requests
+from io import StringIO
 
 
 def get_train_data():
@@ -7,11 +9,13 @@ def get_train_data():
 
 # 下载历史的中奖数据并处理导出CSV到Input文件夹
 def download_lottery_data():
-    url = "http://e.17500.cn/getData/ssq.TXT"
+    url = "https://e.17500.cn/getData/ssq.TXT"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+    data = requests.get(url, headers=headers)
 
     col = ["no", "date", "red_1", "red_2", "red_3", "red_4", "red_5", "red_6", "blue", "red_r_1", "red_r_2", "red_r_3",
            "red_r_4", "red_r_5", "red_r_6"]
-    d = pd.read_table(url, header=None, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], sep=" ")
+    d = pd.read_csv(StringIO(data.text), usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], sep=" ")
     d.columns = pd.Series(col)
     d.to_csv("./input/train_data.csv", index=False)
 
--- train_model.py
+++ train_model.py
@@ -1,7 +1,33 @@
 from get_train_data import *
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+def train_model(train_x, train_y, predict_x):
+    liner = LinearRegression(fit_intercept=True, normalize=False, copy_X=True)
+    liner.fit(train_x, train_y)
+    predict = liner.predict(predict_x)
+    return predict
+
 
 if __name__ == '__main__':
     train_data = get_train_data()
-    train_data.hist(bins=50, figsize=(20, 15))
-    plt.show()
+    col_x = ["red_1", "red_2", "red_3", "red_4", "red_5", "red_6", "blue", "red_r_1", "red_r_2", "red_r_3", "red_r_4",
+             "red_r_5", "red_r_6"]
+    x = train_data.drop(columns=col_x).copy()
+    x["date"] = pd.to_datetime(x["date"])
+
+    col_y = ["no", "date", "red_r_1", "red_r_2", "red_r_3", "red_r_4", "red_r_5", "red_r_6"]
+    y = train_data.drop(columns=col_y, axis=1).copy()
+    for i in y:
+        print("ball number: ")
+        print(y[i])
+
+    df = pd.DataFrame({"no": ["2021095"]})
+    df["date"] = pd.to_datetime(["2021-08-22"])
+
+    print(x)
+    print(y)
+    print(df)
+
+    p = train_model(x, y, df)
+    print(p)
