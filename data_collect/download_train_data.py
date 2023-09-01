eb4bacf1479a88cfe6044bb121aa9b3bcbbb355a
gsxhnd <gsxhnd@gmail.com>
2023-09-01 17:00:10 +0800

    update

--- data_collect/download_train_data.py
+++ data_collect/download_train_data.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import requests
 from io import StringIO
 
 
@@ -8,19 +7,13 @@ def download_lottery_data():
     Download the Lottery Data
     """
 
-    url = "https://e.17500.cn/getData/ssq.TXT"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'
-    }
-    data = requests.get(url, headers=headers)
-
     col = [
         "no", "date",
         "red_1", "red_2", "red_3", "red_4", "red_5", "red_6",
         "blue",
         "red_r_1", "red_r_2", "red_r_3", "red_r_4", "red_r_5", "red_r_6"
     ]
-    d = pd.read_csv(StringIO(data.text), usecols=[
+    d = pd.read_csv("", usecols=[
                     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], sep=" ")
     d.columns = pd.Series(col)
     d.to_csv("./input/train_data.csv", index=False)
